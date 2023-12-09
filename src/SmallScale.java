c02072ba825fc7da1080d953631611f9aa3d6c1d
Bellojacob <jacobbello2002@gmail.com>
2023-12-09 10:27:10 -0800

    Second copy where winning number variable is decided by teams total probability

--- src/SmallScale.java
+++ src/SmallScale.java
@@ -15,14 +15,25 @@ public class SmallScale {
     }
 
     public static void sim(){
+        String winningTeam = null;
+        String secondPick = null;
+        String thirdPick = null;
+        //String fourthPick = null;
+
+        //while (winningTeam != null && secondPick != null && thirdPick != null) {
+
         Random random = new Random();
         HashMap<String, Double> teamMap = new HashMap<String, Double>();
         ArrayList<Integer> allNums = new ArrayList<>();
-        teamMap.put("Spurs", 15.0);
-        teamMap.put("Lakers", 10.0);
-        teamMap.put("Heat", 5.0);
-        int winningNumber = random.nextInt(30)+1;
-        //System.out.println(winningNumber);
+        double spursProb = 15.0;
+        teamMap.put("Spurs", spursProb);
+        double lakersProb = 10.0;
+        teamMap.put("Lakers", lakersProb);
+        double heatProb = 5.0;
+        teamMap.put("Heat", heatProb);
+        double totalProbChance = spursProb + lakersProb + heatProb;
+        int winningNumber = random.nextInt((int) totalProbChance)+1;
+        System.out.println(winningNumber);
 
         for (String i : teamMap.keySet()) {
             String team = i;
@@ -44,14 +55,16 @@ public class SmallScale {
             System.out.println(teamNums.size());
 
         if (teamNums.contains(winningNumber)){
-            System.out.println("The " + team + " has won the first pick in the NBA Draft Lottery!!");
-        } else {
-            continue;
+            // if the current arraylist has the random number that is the winning number, then assign this team as the
+            // winning team, announce the winning team, then we need to remove the winning team from the team Map and
+            // subtract their probability from the possible winning numbers. Then rerun the program again with the teams
+            // remaining and find the second place winning number in the arraylist.
+            winningTeam = team;
+            double winningTeamProb = prob;
+            System.out.println("The " + winningTeam + " has won the first pick in the NBA Draft Lottery!!");
+
+                }
+            }
         }
-        }
-
-
-
-
     }
-}
+//}
